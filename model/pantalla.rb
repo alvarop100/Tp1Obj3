1fb3b54797ea08d2ede49eae58884da71895c386
trimegisto <federico.lochbaum@gmail.com>
2016-03-27 00:02:43 -0300

    agregados

--- model/pantalla.rb
+++ model/pantalla.rb
@@ -7,8 +7,14 @@ class Pantalla
     @ppp=ppp
   end
 
+  #aca para mi hay que revisarlo ehh,intentar hacer como consumo. todo super.
+
   def tasa_refresco
     [tasa_refresco_default,tasa_soportada_por_tecnologia].min
+  end
+
+  def esParaVideoJuegos
+    tasa_refresco > 99
   end
 
 end
